b693e5024351a2a516dc567c69ad1a01ed851407
Misuki-CG <misukicg@gmail.com>
2024-04-18 14:36:33 +0200

    feat(core): adding attribute creator class

---
+++ PoEGraveList/Core/Attribute/AttributeCreator.cs
@@ -0,0 +1,34 @@
+﻿using Newtonsoft.Json.Linq;
+using PoEGraveList.Models;
+using PoEGraveList.Models.Types;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PoEGraveList.Core.Attribute
+{
+    public static class AttributeCreator
+    {
+        public static GameAttribute FromKey(string attributeKey)
+        {
+            GameAttributeType attributeType = Enum.Parse<GameAttributeType>(attributeKey);
+            GameAttribute attribute = new GameAttribute(attributeType);
+
+            return attribute;
+        }
+
+        public static GameAttribute[] FromJObject(JObject attributeObj)
+        {
+            List<GameAttribute> attributeList = new List<GameAttribute>();
+            foreach(KeyValuePair<string, JToken> pair in attributeObj)
+            {
+                attributeList.Add(AttributeCreator.FromKey(pair.Key));
+            }
+
+            return attributeList.ToArray();
+        }
+        
+    }
+}
