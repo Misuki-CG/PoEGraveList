87e74215beb09e541c7eeef452dbacaed835ea1b
Misuki-CG <misukicg@gmail.com>
2024-04-19 08:13:52 +0200

    feat(app) : add minimal view needed

---
+++ PoEGraveList/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,51 @@
+﻿using PoEGraveList.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace PoEGraveList.ViewModels
+{
+    class MainWindowViewModel : ViewModelBase
+    {
+        private string _typedUrl;
+        private ICommand _loadCommand;
+        public string TypedUrl
+        {
+            get
+            {
+                return _typedUrl;
+            }
+            set
+            {
+                _typedUrl = value;
+                OnPropertyChanged(nameof(TypedUrl));
+            }
+        }
+        public ICommand LoadCommand
+        {
+            get
+            {
+                return _loadCommand;
+            }
+            set
+            {
+                _loadCommand = value;
+                OnPropertyChanged(nameof(LoadCommand));
+            }
+        }
+        public MainWindowViewModel() 
+        {
+            this._typedUrl = "https://www.craftofexile.com/?b=20&m=graveyard&ob=both&v=d&a=e&l=a&lg=16&bp=y&as=1&hb=0&req={%221970%22:{%22l%22:82,%22g%22:2},%221972%22:{%22l%22:82,%22g%22:3},%221974%22:{%22l%22:82,%22g%22:1},%221985%22:{%22l%22:73,%22g%22:1}}&bld={}&im={}&ggt=|&ccp={}&gvc={%22weight%22:{%221%22:{%22-300%22:2},%222%22:{%22-300%22:3},%225%22:{%22-300%22:4},%226%22:{%22-300%22:1},%228%22:{%22500%22:2},%2214%22:{%22-300%22:5},%2215%22:{%22500%22:3},%2234%22:{%22500%22:1},%2235%22:{%22500%22:4}},%22tiers%22:{%220%22:{%2250%22:1}},%22prefix%22:{%220%22:{%22300%22:2}},%22suffix%22:{%220%22:{%22300%22:3}},%22explicit%22:{%220%22:{%221%22:4}}}&af={%2229%22:%221%22,%2233%22:%221%22}";
+            this._loadCommand = new BaseCommand((obj) => this.loadQuery());
+        }
+
+
+        private void loadQuery()
+        {
+
+        }
+    }
+}
